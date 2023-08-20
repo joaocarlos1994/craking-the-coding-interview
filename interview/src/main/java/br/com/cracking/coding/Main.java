023aca782f8ed3dd2be48870cbc39886889263c0
joaocarlos1994 <joao.carl98@gmail.com>
2023-08-20 13:01:12 -0300

    First commit

---
+++ interview/src/main/java/br/com/cracking/coding/Main.java
@@ -0,0 +1,11 @@
+package br.com.cracking.coding;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("This repository has as common goal to provide " +
+                "and share example of code from Cracking the Coding Interview book and " +
+                " look some test code from leetcode.");
+    }
+}
