99af2e36c84042935eb8c3182ea2b8e30cf183e3
joaocarlos1994 <joao.carl98@gmail.com>
2023-08-21 22:00:00 -0300

    Add exercises

---
+++ src/main/java/br/com/cracking/coding/strings/Exercise03URLify.java
@@ -0,0 +1,43 @@
+package br.com.cracking.coding.strings;
+
+import org.junit.jupiter.api.Assertions;
+
+public class Exercise03URLify {
+
+    /**
+     * Write a method to replace all spaces in a string with %20. You may assume that the string has sufficient
+     * space at the end to hold additional characters, and that you are given the 'true' length of the string
+     * (Note: if implementation in Java, please use character array so that you can perform this operation in
+     * place.)
+     * */
+
+    public static void main(String[] args) {
+        String word = "Mr John Smith    ";
+        Assertions.assertEquals("Mr%20John%20Smith", replaceSpaces(word.toCharArray(), 13));
+    }
+
+    public static String replaceSpaces(char[] str, int trueLength) {
+        int spaceCount = 0, index, i = 0;
+        for (i = 0; i < trueLength; i++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
+        }
+        index = trueLength + spaceCount * 2;
+        if (trueLength < str.length) {
+            str[trueLength] = '\0';
+        }
+        for (i = trueLength - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                str[index - 1] = '0';
+                str[index - 2] = '2';
+                str[index - 3] = '%';
+                index = index - 3;
+            } else {
+                str[index - 1] = str[i];
+                index--;
+            }
+        }
+        return new String(str);
+    }
+}
