7678b01fa91f160866f6616e5c122211beef7e4c
joaocarlos1994 <joao.carl98@gmail.com>
2023-08-29 14:00:26 -0300

    Add exercises

--- src/main/java/br/com/cracking/coding/strings/Exercise05EditDistanceLeetCode.java
+++ src/main/java/br/com/cracking/coding/strings/Exercise05EditDistanceLeetCode.java
@@ -13,47 +13,40 @@ import java.util.Map;
 public class Exercise05EditDistanceLeetCode {
     public static void main(String[] args) {
         int i = minDistance("horse", "ros");
-        //int j = minDistance("intention", "execution");
+        int j = minDistance("intention", "execution");
         int k = minDistance("ab", "a");
-        //Assertions.assertEquals(3, i);
-        //Assertions.assertEquals(5, j);
+        Assertions.assertEquals(3, i);
+        Assertions.assertEquals(5, j);
         Assertions.assertEquals(1, k);
     }
 
     public static int minDistance(String word1, String word2) {
-        final Map<Character, Integer> aux = new HashMap<>();
-        for (int i = 0; i < word2.length(); i++) {
-            char letter = word2.charAt(i);
-            if (aux.containsKey(letter)) {
-                int count = aux.get(letter);
-                aux.put(letter, count++);
-            } else {
-                aux.put(letter, 1);
-            }
+        int[][] result = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) {
+            result[i][0] = i;
         }
 
-        int result = 0;
-        int indexS2 = 0;
-        for (int i = 0; i < word1.length(); i++) {
-            char letter = word1.charAt(i);
-            if (aux.containsKey(letter)) {
-                char target = word2.charAt(indexS2);
-                if (!(letter == target)) {
-                    result++;
-                }
+        for (int i = 0; i <= word2.length(); i++) {
+            result[0][i] = i;
+        }
 
-                indexS2++;
+        for (int i = 1; i < result.length; i++) {
+            for (int j = 1; j < result[i].length; j++) {
+                char letterWord1 = word1.charAt(i - 1);
+                char letterWord2 = word2.charAt(j - 1);
 
-                Integer countLetter = aux.get(letter);
-                if (countLetter - 1 == 0) {
-                    aux.remove(letter);
+                int value1 = result[i -1][j];
+                int value2 = result[i -1][j - 1];
+                int value3 = result[i][j -1];
+
+                int resultValue = Math.min(value3, Math.min(value1, value2));
+                if (letterWord1 == letterWord2) {
+                    result[i][j] =  result[i - 1][j - 1];
                 } else {
-                    aux.put(letter, countLetter--);
+                    result[i][j] = resultValue + 1;
                 }
-            } else {
-                result++;
             }
         }
-        return result;
+        return result[word1.length()][word2.length()];
     }
 }
