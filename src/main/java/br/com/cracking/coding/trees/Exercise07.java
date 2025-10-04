fa6dedb7c8e7263c73cb24258729d559be3372a6
joaocarlos1994 <joao.carl98@gmail.com>
2025-10-04 19:01:46 -0300

    Add some code

--- src/main/java/br/com/cracking/coding/trees/Exercise07.java
+++ src/main/java/br/com/cracking/coding/trees/Exercise07.java
@@ -1,8 +1,9 @@
 package br.com.cracking.coding.trees;
 
-import org.junit.jupiter.api.Assertions;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Exercise07 {
 
