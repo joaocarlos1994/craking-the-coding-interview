30dc07177d882e0bdad6486a4187afa4e7e4aa28
joaocarlos1994 <joao.carl98@gmail.com>
2025-10-04 19:01:35 -0300

    Add some code

---
+++ src/main/java/br/com/cracking/coding/trees/Exercise07.java
@@ -0,0 +1,55 @@
+package br.com.cracking.coding.trees;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.util.*;
+
+public class Exercise07 {
+
+    public static void main(String[] args) {
+        Exercise07 exercise07 = new Exercise07();
+        exercise07.findOrder(4, new int[][]{{1, 0}, {2, 0}, {3, 1}, {3, 2}});
+    }
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+
+        int[] total = new int[numCourses];
+        for (int[] values : prerequisites) {
+            graph.get(values[1]).add(values[0]);
+            total[values[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (total[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        List<Integer> result = new LinkedList<>();
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            result.add(node);
+            List<Integer> neighbor = graph.get(node);
+            for (int i = 0; i < neighbor.size(); i++) {
+                total[neighbor.get(i)]--;
+                if (total[neighbor.get(i)] == 0) {
+                    queue.add(neighbor.get(i));
+                }
+            }
+        }
+
+        if (result.size() == numCourses) {
+            int[] values = new int[result.size()];
+            for (int i = 0; i < result.size(); i++) {
+                values[i] = result.get(i);
+            }
+            return values;
+        }
+        return new int[]{};
+    }
+}
